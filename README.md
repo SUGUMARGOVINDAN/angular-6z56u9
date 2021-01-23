8697e9c18921247b0492d75b423422a9cfebc1a3
SUGUMARGOVINDAN <sugumargovindan78@gmail.com>
2021-01-23 13:58:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-6z56u9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-6z56u9)
---
+++ src/app/products.ts
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Appple',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    id: 2,
+    name: 'Orange',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    id: 3,
+    name: 'Bag',
+    price: 299,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
